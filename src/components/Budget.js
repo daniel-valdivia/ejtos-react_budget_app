b433a7e599b63b19eb8b01ac831bbac48df656ec
Daniel Valdivia <valdiviajrdaniel@gmail.com>
2024-04-16 12:03:48 -0400

    Fixed Budget change and added alerts for max and min budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,16 +1,52 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
+
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+
+    // Update local state when budget changes in context
+    useEffect(() => {
+        setNewBudget(budget);
+    }, [budget]);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        const updatedBudget = parseInt(event.target.value, 10); // Parse input value as integer
+        setNewBudget(updatedBudget); // Update local state
+    };
+
+    const handleSaveBudget = () => {
+        // Dispatch action to update budget in context
+        if(newBudget > 20000) {
+            alert('Budget max is 20,000');
+            setNewBudget("");
+            return;
+        }
+        if(newBudget < totalExpenses) {
+            alert('You cannot reduce the budget value lower than the spending');
+            setNewBudget("");
+            return;
+        }   
+        dispatch({ type: 'SET_BUDGET', payload: newBudget });
+    };
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £</span>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+            />
+            <button onClick={handleSaveBudget}>Save</button>
+        </div>
     );
 };
+
 export default Budget;
